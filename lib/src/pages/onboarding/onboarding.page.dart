cd2393d73e12715761ec4573a80a34c7063a79b6
Bin Emmanuel <textme@binemmanuel.com>
2025-12-09 16:19:03 +0100

    Changed system ui overlay style

--- lib/src/pages/onboarding/onboarding.page.dart
+++ lib/src/pages/onboarding/onboarding.page.dart
@@ -1,18 +1,27 @@
-import 'package:flutter/services.dart' show SystemUiOverlayStyle;
+import 'package:flutter/services.dart' show SystemUiOverlayStyle, SystemChrome;
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/widgets.dart'
     show BottomSlideShadow, WelcomeMessage, InfiniteSlides, TopSlideShadow;
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class OnboardingPage extends HookWidget {
+  const OnboardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final brightness = Theme.brightnessOf(context);
+
+    useEffect(() {
+      return () {
+        if (brightness == .dark) return;
+        SystemChrome.setSystemUIOverlayStyle(.dark);
+      };
+    }, []);
+
     return const Scaffold(
       body: AnnotatedRegion(
         value: SystemUiOverlayStyle.light,
-
         child: Column(
           children: [
             Expanded(
