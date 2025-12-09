bf82218125557e2f8555ccb5fc02963cfd8683a2
Bin Emmanuel <textme@binemmanuel.com>
2025-12-09 16:20:10 +0100

    Added new export

--- lib/src/widgets/widgets.dart
+++ lib/src/widgets/widgets.dart
@@ -2,3 +2,9 @@ export '../pages/onboarding/widgets/bottom.slide.shadow.dart';
 export '../pages/onboarding/widgets/top.slide.shadow.dart';
 export '../pages/onboarding/widgets/welcome.message.dart';
 export '../pages/onboarding/widgets/infinite.slides.dart';
+
+export '../pages/home/widgets/ask.anything.text.field.dart';
+export '../pages/home/widgets/quick.chat.options.dart';
+export '../pages/home/widgets/bottom.navigator.dart';
+export '../pages/home/widgets/home.app.bar.dart';
+export '../pages/home/widgets/quick.option.dart';
